ac7c18b7aeabd625310f123c09e3bf60db198fcc
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2023-09-26 23:48:26 -0500

    adding js generate password

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,13 +1,45 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+const generatePassword = () => {
+  var length = parseInt(
+    prompt(
+      "How many characters would you like your password? Choose between 8 and 128"
+    )
+  );
+
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please insert a valid number between 8 and 128.");
+    return "";
+  }
+
+  var lowercase = confirm("Do you want to include lowercase characters?");
+  var uppercase = confirm("Do you want to include uppercase characters?");
+  var numbers = confirm("Do you want to include numeric characters?");
+  var specialChars = confirm("Do you want to include special characters?");
+
+  if (!lowercase && !uppercase && !numbers && !specialChars) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  var char = "";
+  if (lowercase) char += "abcdefghijklmnopqrstuvwxyz";
+  if (uppercase) char += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbers) char += "0123456789";
+  if (specialChars) char += "!@#$%^&*()_+";
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var random = Math.floor(Math.random() * char.length);
+    password += char.charAt(random);
+  }
+  return password;
+};
+
 generateBtn.addEventListener("click", writePassword);
